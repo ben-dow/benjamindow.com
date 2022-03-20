1ca821f7bd0d01751e46ec4cbc06b58990529040
Ben Dow <benjamindow1@gmail.com>
2022-03-20 15:01:18 -0400

    SSL for pdf

--- src/Resume.js
+++ src/Resume.js
@@ -2,7 +2,7 @@ export default function Resume() {
   return (
     <div className={"w-full h-full"}>
       <embed
-        src="http://resume.benjamin-dow.com/BenjaminDowResume.pdf"
+        src="https://resume.benjamin-dow.com/BenjaminDowResume.pdf"
         className={"w-full h-full"}
         type="application/pdf"
       />
