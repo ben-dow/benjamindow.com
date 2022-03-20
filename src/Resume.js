d15e86e4a5000a0dc4b98bcf807c88b6fe09e896
Ben Dow <benjamindow1@gmail.com>
2022-03-20 15:45:35 -0400

    resume update

--- src/Resume.js
+++ src/Resume.js
@@ -3,9 +3,20 @@ export default function Resume() {
     <div className={"w-full h-full"}>
       <embed
         src="https://resume.benjamin-dow.com/BenjaminDowResume.pdf"
-        className={"w-full h-full"}
+        className={"w-full h-full hidden sm:flex"}
         type="application/pdf"
       />
+      <div
+        className={
+          "sm:hidden w-full p-2 flex bg-slate-700 bg-opacity-75 text-white font-semibold mt-5 justify-center"
+        }
+      >
+        <div className={"bg-blue-500 flex p-3 "}>
+          <a href={"https://resume.benjamin-dow.com/BenjaminDowResume.pdf"}>
+            View Resume
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
